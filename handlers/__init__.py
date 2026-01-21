c988294a337402b932c1f80bda1035dbdfc74be3
simonscats69-code <simonscats69@gmail.com>
2026-01-22 01:53:06 +0300

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,3 +1,4 @@
+from aiogram import Router
 from .commands import router as commands_router
 from .callbacks import router as callbacks_router
 from .daily import router as daily_router
@@ -5,8 +6,15 @@ from .nickname_and_rademka import router as nickname_router
 from .shop import router as shop_router
 from .top import router as top_router
 from .atm_handlers import router as atm_router
-from aiogram import Router
 
 router = Router()
+
+router.include_router(commands_router)
+router.include_router(callbacks_router)
+router.include_router(daily_router)
+router.include_router(nickname_router)
+router.include_router(shop_router)
+router.include_router(top_router)
+router.include_router(atm_router)
 
 __all__ = ['router']
