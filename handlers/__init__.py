6e622dc5068bd66b5527ede258342ffba08b2d42
simonscats69-code <simonscats69@gmail.com>
2026-01-22 21:09:55 +0300

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -4,6 +4,7 @@ from .callbacks import router as callbacks_router
 from .nickname_and_rademka import router as nickname_router
 from .top import router as top_router
 from .atm_handlers import router as atm_router
+from .group_handlers import router as group_router
 
 router = Router()
 
@@ -12,5 +13,6 @@ router.include_router(callbacks_router)
 router.include_router(nickname_router)
 router.include_router(top_router)
 router.include_router(atm_router)
+router.include_router(group_router)
 
 __all__ = ['router']
